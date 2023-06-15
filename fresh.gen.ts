db916be57936a60d4857bd32b8b426e904524ff6
Simone <xsimone97@hotmail.it>
2023-06-15 19:32:04 +0200

    types

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -9,12 +9,15 @@ import * as $2 from "./routes/api/get-matches.ts";
 import * as $3 from "./routes/api/joke.ts";
 import * as $4 from "./routes/api/random-uuid.ts";
 import * as $5 from "./routes/api/submit-solutions.ts";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/test.tsx";
-import * as $8 from "./routes/username.tsx";
+import * as $6 from "./routes/gameover.tsx";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/test.tsx";
+import * as $9 from "./routes/username.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Game.tsx";
-import * as $$2 from "./islands/Timer2.tsx";
+import * as $$2 from "./islands/GameOver.tsx";
+import * as $$3 from "./islands/SetUser.tsx";
+import * as $$4 from "./islands/Timer2.tsx";
 
 const manifest = {
   routes: {
@@ -24,14 +27,17 @@ const manifest = {
     "./routes/api/joke.ts": $3,
     "./routes/api/random-uuid.ts": $4,
     "./routes/api/submit-solutions.ts": $5,
-    "./routes/index.tsx": $6,
-    "./routes/test.tsx": $7,
-    "./routes/username.tsx": $8,
+    "./routes/gameover.tsx": $6,
+    "./routes/index.tsx": $7,
+    "./routes/test.tsx": $8,
+    "./routes/username.tsx": $9,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
     "./islands/Game.tsx": $$1,
-    "./islands/Timer2.tsx": $$2,
+    "./islands/GameOver.tsx": $$2,
+    "./islands/SetUser.tsx": $$3,
+    "./islands/Timer2.tsx": $$4,
   },
   baseUrl: import.meta.url,
   config,
