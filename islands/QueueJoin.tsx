793753a450e453cde89fe8c01f528c2cda5e094f
Gianluca <g1an@ymail.com>
2023-06-15 20:30:12 +0200

    countergrafica

--- islands/QueueJoin.tsx
+++ islands/QueueJoin.tsx
@@ -8,32 +8,30 @@ export default function QueueJoin() {
   const [matchesIndex, setMatchesIndex] = useState(0);
 
   useEffect(() => {
-    fetch(
-      `/api/get-matches`,
-    )
+    fetch(`/api/get-matches`)
       .then((response) => response.json())
       .then((data) => {
-          setMatches(data);
+        setMatches(data);
         //   console.log(data);
-          //set a timer for when the first match will start
-            const now = Date.now();
-            // get the first match in the future
-            let firstMatch = data[0];
-            for (let i = 0; i < data.length; i++) {
-              if (data[i] > now) {
-                firstMatch = data[i];
-                break;
-              }
-            }
-            // set the timer
-            const timer = setInterval(() => {
-                const now = Date.now();
-                if (firstMatch > now) {
-                    setCountdown(Math.round((firstMatch - now) / 1000));
-                } else {
-                    setMatch(firstMatch);
-                }
-            });
+        //set a timer for when the first match will start
+        const now = Date.now();
+        // get the first match in the future
+        let firstMatch = data[0];
+        for (let i = 0; i < data.length; i++) {
+          if (data[i] > now) {
+            firstMatch = data[i];
+            break;
+          }
+        }
+        // set the timer
+        const timer = setInterval(() => {
+          const now = Date.now();
+          if (firstMatch > now) {
+            setCountdown(Math.round((firstMatch - now) / 1000));
+          } else {
+            setMatch(firstMatch);
+          }
+        });
       })
       .catch((_error) => {
         console.log("error");
@@ -42,30 +40,32 @@ export default function QueueJoin() {
 
   useEffect(() => {
     const timer = setInterval(() => {
-        const now = Date.now();
-        if (now > matches[matchesIndex]) {
-            setMatchesIndex(matchesIndex + 1);
-            if (queueJoined){
-                // go to game
-                window.location.href = `/${matches[matchesIndex]}`;
-            } else {
-                //TODO: do api get-matches
-            }
+      const now = Date.now();
+      if (now > matches[matchesIndex]) {
+        setMatchesIndex(matchesIndex + 1);
+        if (queueJoined) {
+          // go to game
+          window.location.href = `/${matches[matchesIndex]}`;
         } else {
-            setCountdown(Math.round((matches[matchesIndex] - now) / 1000));
+          //TODO: do api get-matches
         }
-      }, 1000);
-      return () => clearInterval(timer);
-    }, [countdown, matches, matchesIndex, queueJoined]);
+      } else {
+        setCountdown(Math.round((matches[matchesIndex] - now) / 1000));
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [countdown, matches, matchesIndex, queueJoined]);
 
   return (
     <>
       <div>
-        <div class="flex justify-center ">{countdown}</div>
+        <div class="flex justify-center text-2xl text-blue-800 font-bold ">
+          <p>Next match in: {countdown}</p>
+        </div>
         <button
           class={`px-3 py-2 text-xl text-gray-50 rounded m-5 h-20 w-60 ${
             queueJoined
-              ? "bg-gray-400 opacity-50 cursor-not-allowed"
+              ? "bg-blue-400 opacity-50 cursor-not-allowed"
               : "bg-blue-800 hover:bg-blue-500"
           }`}
           disabled={queueJoined}
